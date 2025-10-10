ca38ecce311b26cc0a3098c8a1830f868201c7ea
kevin-frss-epi <kevin.forsans@epitech.eu>
2025-10-10 11:11:14 +0200

    [KAN-28] FEAT: Swagger setup dquote> Add minimal documentation of the user testing routes dquote> Fix backend Dockerfile configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,34 @@ import morgan from "morgan";
 
 import usersRoutes from "./routes/userRoutes.js";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
+
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    myapi: '3.0.0',
+    info: {
+      title: 'Time manager API',
+      version: '1.0.0',
+      description: 'API documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/users", usersRoutes);
 
