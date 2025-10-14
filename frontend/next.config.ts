a51cd9205c9c0aaa51e35bb1fa727146965a8c5e
Lucas Abadie <lucasabadie.pro@gmail.com>
2025-10-14 17:24:44 +0200

    [KAN-95] FEAT: Configuration Reverse Proxy NGINX
    - Remove redirection in next.config.ts
    - Add all server configuration (Frontend, Backend, PGAdmin)
    - Update docker compose with the new service & clean it
    - Rename the frontend Dockerfile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,7 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',                
-        destination: 'http://backend:3000/:path*',
-      },
-    ];
-  },
 
-  telemetry: false,
-  reactStrictMode: true,
 };
 
 export default nextConfig;
