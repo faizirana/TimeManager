88efd438a752981f0dfcd1c80bd07f06fd0aae6d
gabrielbasso <gabriel.basso@epitech.eu>
2025-11-06 17:00:56 +0100

    KAN-66 [FEAT] TeamRoutes + swagger

--- backend/migrations/20250001-create-users.cjs
+++ backend/migrations/20250001-create-users.cjs
@@ -1,40 +1,40 @@
 // migrations/xxxx-create-user.js
 module.exports = {
   //Initialization of the tables and effective creation in the database in the table.
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, _Sequelize) => {
     await queryInterface.createTable('User', {
       id: {
-        type: Sequelize.INTEGER,
+        type: _Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
       name: {
-        type: Sequelize.STRING,
+        type: _Sequelize.STRING,
         allowNull: false,
       },
       surname :{
-        type: Sequelize.STRING,
+        type: _Sequelize.STRING,
         allowNull: false,
       },
       mobileNumber: {
-      type: Sequelize.STRING,
+      type: _Sequelize.STRING,
       allowNull: false,
       },
       email: {
-      type: Sequelize.STRING,
+      type: _Sequelize.STRING,
       allowNull: false,
       unique: true,
       },
       password: {
-      type: Sequelize.STRING,
+      type: _Sequelize.STRING,
       allowNull: false,
       },
       role: {
-      type: Sequelize.ENUM('manager', 'employee'),
+      type: _Sequelize.ENUM('manager', 'employee'),
       allowNull: false,
       },
       id_manager: {
-      type: Sequelize.INTEGER,
+      type: _Sequelize.INTEGER,
       allowNull: true,
       references: {
         model: 'User',
@@ -45,7 +45,7 @@ module.exports = {
   },
   //When the down command is executate we destruct the table.
   //In fact, when the network or the Docker is down the table is drop.
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable('User');
   },
 };
